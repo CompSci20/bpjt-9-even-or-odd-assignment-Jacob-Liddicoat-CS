c8661f66edb504f9916d6becbc8ff1c8dfc4deec
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-17 20:07:15 +0000

    Finish Even or Odd

---
+++ Tester.java
@@ -0,0 +1,45 @@
+//Author: Jacob Liddicoat
+//Date created: Mar 17,2018
+import java.io.*;
+import java.util.*;
+public class Tester
+{
+ public static void main(String args[])
+ {
+  Scanner kbReader = new Scanner(System.in);
+  System.out.print("28");
+  int theInteger = kbReader.nextInt();
+  if (theInteger % 2 == 0)
+   {
+    System.out.println("The Integer" + theInteger + "is Even.");
+   }
+   else
+   {
+    System.out.println("The Integer" + theInteger + "is Odd.");
+   }
+
+    Scanner kbReader = new Scanner(System.in);
+  System.out.print("2049");
+  int theInteger = kbReader.nextInt();
+  if (theInteger % 2 == 0)
+   {
+    System.out.println("The Integer" + theInteger + "is Even.");
+   }
+   else
+   {
+    System.out.println("The Integer" + theInteger + "is Odd.");
+   }
+
+    Scanner kbReader = new Scanner(System.in);
+  System.out.print("-236 ");
+  int theInteger = kbReader.nextInt();
+  if (theInteger % 2 == 0)
+   {
+    System.out.println("The Integer" + theInteger + "is Even.");
+   }
+   else
+   {
+    System.out.println("The Integer" + theInteger + "is Odd.");
+   }
+ }
+}
